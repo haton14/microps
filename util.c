d2a8c5def7eacb350bed533bc48b07b222895d1a
haton14 <haton14@haton14.com>
2023-08-13 19:22:57 +0900

    format

--- util.c
+++ util.c
@@ -47,24 +47,27 @@ hexdump(FILE *fp, const void *data, size_t size)
     flockfile(fp);
     src = (unsigned char *)data;
     fprintf(fp, "+------+-------------------------------------------------+------------------+\n");
-    for(offset = 0; offset < (int)size; offset += 16) {
+    for (offset = 0; offset < (int)size; offset += 16) {
         fprintf(fp, "| %04x | ", offset);
-        for(index = 0; index < 16; index++) {
-            if(offset + index < (int)size) {
+        for (index = 0; index < 16; index++) {
+            if (offset + index < (int)size) {
                 fprintf(fp, "%02x ", 0xff & src[offset + index]);
-            } else {
+            }
+            else {
                 fprintf(fp, "   ");
             }
         }
         fprintf(fp, "| ");
-        for(index = 0; index < 16; index++) {
-            if(offset + index < (int)size) {
-                if(isascii(src[offset + index]) && isprint(src[offset + index])) {
+        for (index = 0; index < 16; index++) {
+            if (offset + index < (int)size) {
+                if (isascii(src[offset + index]) && isprint(src[offset + index])) {
                     fprintf(fp, "%c", src[offset + index]);
-                } else {
+                }
+                else {
                     fprintf(fp, ".");
                 }
-            } else {
+            }
+            else {
                 fprintf(fp, " ");
             }
         }
@@ -181,7 +184,7 @@ byteorder(void) {
 static uint16_t
 byteswap16(uint16_t v)
 {
-    return (v & 0x00ff) << 8 | (v & 0xff00 ) >> 8;
+    return (v & 0x00ff) << 8 | (v & 0xff00) >> 8;
 }
 
 static uint32_t
